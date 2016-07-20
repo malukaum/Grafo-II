283837b5be517499c6b01e0e3b5977a9db7dce33
malukaum <otavio.a.chaves@gmail.com>
2016-07-20 00:46:34 -0300

    Add files via upload

--- grafo.c
+++ grafo.c
@@ -27,14 +27,36 @@ struct grafo {
     ListaArestas pa ;
 };
 
-Grafo GGcriaGrafo(int sementeVertice, int sementeAresta){
+ListaVertices findVertice(Grafo grafo,int idVertice){
+    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
+    aux=grafo->pv;
+    while (aux->proximo != NULL){
+        if (aux->idVertice==idVertice){
+            return aux;
+        }
+        aux=aux->proximo;
+    }
+}
+
+ListaArestas findAresta(Grafo grafo,int idAresta){
+    ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
+    aux=grafo->pa;
+    while (aux->proximo != NULL){
+        if (aux->idAresta==idAresta){
+            return aux;
+        }
+        aux=aux->proximo;
+    }
+}
+
+Grafo GGcriaGrafo(){
     Grafo grafo = (Grafo) malloc(sizeof(Grafo));
     grafo->numArestas=0;
     grafo->numVertices=0;
     grafo->pa=NULL;
     grafo->pv=NULL;
-    grafo->sementeArestas=sementeAresta;
-    grafo->sementeVertices=sementeVertice;
+    grafo->sementeArestas=0;
+    grafo->sementeVertices=0;
     return grafo;
 };
 
@@ -49,19 +71,19 @@ int GVcriaVertice(Grafo grafo, int idVertice){
     if (grafo->pv==NULL){
         grafo->pv=vertice;
     }else{
-        while (aux <> NULL){
+        while (aux->proximo != NULL){
             aux=aux->proximo;
         }
         aux->proximo=vertice;
     }
-    return grafo->numVertices;
+    return vertice->idVertice;
 }
 
 int GAcriaAresta(Grafo grafo, int idAresta, int alfa, int omega){
     grafo->numArestas++;
     ListaArestas aresta = (ListaArestas) malloc(sizeof(ListaArestas));
-    aresta->alfa=findVertice(alfa);
-    aresta->omega=findVertice(omega);
+    aresta->alfa=findVertice(grafo,alfa);
+    aresta->omega=findVertice(grafo,omega);
     aresta->idAresta=idAresta;
     aresta->proximo=NULL;
     if (grafo->pa==NULL){
@@ -69,45 +91,65 @@ int GAcriaAresta(Grafo grafo, int idAresta, int alfa, int omega){
     }else{
         ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
         aux=grafo->pa;
-        while (aux->proximo<>NULL){
+        while (aux->proximo!=NULL){
             aux=aux->proximo;
         }
         aux->proximo=aresta;
     }
     ListaEstrelas estrela = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
     estrela->proximo=NULL;
-    estrela->aresta=aresta;
-    if (aresta->alfa->estrela==NULL){
+    estrela->aresta=aresta;//cria novo no estrela
+    if (aresta->alfa->estrela==NULL){//verifica se o alfa ja possui ao menos uma estrela
         aresta->alfa->estrela=estrela;
     }else{
         ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
         auxE=aresta->alfa->estrela;
-        while(auxE->proximo<>NULL){
+        while(auxE->proximo!=NULL){
             auxE=auxE->proximo;
         }
         auxE->proximo=estrela;
+        if (aresta->omega->estrela==NULL){
+            aresta->omega->estrela=estrela;
+        }else{
+            auxE=aresta->omega->estrela;
+            while(auxE->proximo!=NULL){
+                auxE=auxE->proximo;
+            }
+            auxE->proximo=estrela;
+        }
     }
     return aresta->idAresta;
 }
 
-ListaVertices findVertice(Grafo grafo,int idVertice){
-    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
-    aux=grafo->pv;
-    while (aux->proximo <> NULL){
-        if (aux->idVertice==idVertice){
-            return aux;
+Grafo GGcarregaGrafo(char caminho[]){
+    int j, x, y, i = 0;
+    Grafo grafo; //= (Grafo) malloc(sizeof(Grafo));
+    grafo=GGcriaGrafo();
+    FILE *arq = fopen(caminho, "r");
+    if (arq == NULL)
+    {
+        fclose(arq);
+        return 0;
+    }else {
+        while( (fscanf(arq,"%d%d%d",&j,&x,&y))!=EOF ){
+            if (i==0){
+                grafo->numVertices=j;
+                grafo->numArestas=x;
+            }else if (i==1){
+                grafo->sementeVertices=j;
+                grafo->sementeArestas=x;
+            }else if ((i>1)&&(i<grafo->numVertices+2)){
+                GVcriaVertice(grafo,j);
+            }else{
+                GAcriaAresta(grafo,j,x,y);
+            }
+            i++;
         }
+        fclose(arq);
+        return grafo;
     }
-}
-
-ListaArestas findAresta(Grafo grafo,int idAresta){
-    ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pa;
-    while (aux->proximo <> NULL){
-        if (aux->idAresta==idAresta){
-            return aux;
-        }
-    }
+    fclose(arq);
+    return 0;
 }
 //=====================================================
 // Função que verifica a existência de um vértice por 
@@ -116,7 +158,7 @@ ListaArestas findAresta(Grafo grafo,int idAresta){
 int GBexisteIdVertice (Grafo grafo, int idVertice){
     ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
     aux=grafo->pv;
-    while (aux <> NULL){
+    while (aux !=NULL){
         if (aux->idVertice==idVertice){
             return 1;
         }
@@ -130,8 +172,8 @@ int GBexisteIdVertice (Grafo grafo, int idVertice){
 //=====================================================
 int GBexisteIdAresta (Grafo grafo, int idAresta){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if (aux->idAresta==idAresta){
             return 1;
         }
@@ -145,8 +187,8 @@ int GBexisteIdAresta (Grafo grafo, int idAresta){
 //=====================================================
 int GBexisteArestaDir (Grafo grafo, int x, int y){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if ((aux->alfa->idVertice==x)&&(aux->omega->idVertice==y)){
             return 1;
         }
@@ -160,8 +202,8 @@ int GBexisteArestaDir (Grafo grafo, int x, int y){
 //=====================================================
 int GBexisteAresta (Grafo grafo, int x, int y){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if (((aux->alfa->idVertice==x)&&(aux->omega->idVertice==y))||
                 ((aux->alfa->idVertice==y)&&(aux->omega->idVertice==x))){
             return 1;
@@ -175,8 +217,8 @@ int GBexisteAresta (Grafo grafo, int x, int y){
 //=====================================================
 int GApegaArestaDir(Grafo grafo, int x, int y){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if ((aux->alfa->idVertice==x)&&(aux->omega->idVertice==y)){
             return aux->idAresta;
         }
@@ -189,8 +231,8 @@ int GApegaArestaDir(Grafo grafo, int x, int y){
 //=====================================================
 int GApegaAresta(Grafo grafo, int x, int y){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if (((aux->alfa->idVertice==x)&&(aux->omega->idVertice==y))||
                 ((aux->alfa->idVertice==y)&&(aux->omega->idVertice==x))){
             return aux->idAresta;
@@ -203,7 +245,7 @@ int GApegaAresta(Grafo grafo, int x, int y){
 // Função que pega o primeiro vértice
 //=====================================================
 int GVprimeiroVertice(Grafo grafo){
-    if (grafo->pv<>NULL){
+    if (grafo->pv!=NULL){
         return grafo->pv->idVertice;
     }else return 0;
 }
@@ -213,7 +255,7 @@ int GVprimeiroVertice(Grafo grafo){
 int GVproximoVertice (Grafo grafo, int idVertice){
     ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
     aux=findVertice(grafo,idVertice);
-    if (aux->proximo<>NULL){
+    if (aux->proximo!=NULL){
         return aux->proximo->idVertice;
     }else return 0;
 }
@@ -221,7 +263,7 @@ int GVproximoVertice (Grafo grafo, int idVertice){
 // Função que pega o ID da primeira aresta
 //=====================================================
 int GAprimeiraAresta(Grafo grafo){
-    if (grafo->pa<>NULL){
+    if (grafo->pa!=NULL){
         return grafo->pa->idAresta;
     }else return 0;
 }
@@ -231,7 +273,7 @@ int GAprimeiraAresta(Grafo grafo){
 int GAproximaAresta(Grafo grafo, int idAresta){
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
     aux=findAresta(grafo,idAresta);
-    if (aux->proximo<>NULL){
+    if (aux->proximo!=NULL){
         return aux->proximo->idAresta;
     }else return 0;
 }
@@ -246,7 +288,7 @@ int GInumeroVertices(Grafo grafo){
 // grafo aceita
 //=====================================================
 int GInumeroVerticesMax(Grafo grafo){
-    //a implementar
+    ////////////////////////////////////////////////////////////a imlementar
 }
 //=====================================================
 // Função que pega o número de arestas
@@ -259,7 +301,7 @@ int GInumeroArestas(Grafo grafo){
 // grafo suporta
 //=====================================================
 int GInumeroArestasMax(Grafo grafo){
-    //a imlementar
+    ////////////////////////////////////////////////////////////a imlementar
 }
 //=====================================================
 // Função que salva o grafo em um arquivo
@@ -271,7 +313,7 @@ int GBsalvaGrafo(Grafo grafo, char caminho[]){
         fclose(arq);
         return 0;
     }else{
-        //a implementar
+        ////////////////////////////////////////////////////////a imlementar
         fclose(arq);
         return 1;
     }
@@ -282,8 +324,8 @@ int GBsalvaGrafo(Grafo grafo, char caminho[]){
 int GIpegaGrau(Grafo grafo, int vertice){
     int grau=0;
     ListaArestas aux = (ListaArestas) malloc(sizeof(ListaArestas));
-    aux=grafo->pv;
-    while (aux <> NULL){
+    aux=grafo->pa;
+    while (aux != NULL){
         if ((aux->alfa->idVertice==vertice)||(aux->omega->idVertice==vertice)){
             grau++;
             if ((aux->alfa->idVertice==vertice)&&(aux->omega->idVertice==vertice)){
@@ -297,23 +339,23 @@ int GIpegaGrau(Grafo grafo, int vertice){
 // Função que pega a primeira aresta na estrela do 
 // vértice
 //=====================================================
-ListaArestas GAprimaAresta(Grafo grafo, int vertice){
+int GAprimaAresta(Grafo grafo, int vertice){
    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
-   aux=findVertice(vertice);
-   return aux->estrela->aresta;
+   aux=findVertice(grafo,vertice);
+   return aux->estrela->aresta->idAresta;
 }
 //=====================================================
 // Função que pega a próxima aresta na estrela do 
 // vértice
 //=====================================================
-ListaArestas GAproxAresta(Grafo grafo, int vertice, int aresta){
+int GAproxAresta(Grafo grafo, int vertice, int aresta){
    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
-   aux=findVertice(vertice);
+   aux=findVertice(grafo,vertice);
    ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
    auxE=aux->estrela;
-   while (auxE<>NULL){
+   while (auxE!=NULL){
        if (auxE->aresta->idAresta==aresta){
-           return auxE->proximo->aresta;
+           return auxE->proximo->aresta->idAresta;
        }
    }
    return 0;
@@ -323,23 +365,33 @@ ListaArestas GAproxAresta(Grafo grafo, int vertice, int aresta){
 // entrada de um vértice 
 //=====================================================
 int GAprimaEntrada(Grafo grafo, int vertice){
-    for(i=0;i<grafo->NumAre;i++){
-        if(grafo->aresta[i].y==vertice)
-            return grafo->aresta[i].id;
-    }
-    return 0;
+    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
+    aux=findVertice(grafo,vertice);
+    ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
+    auxE=aux->estrela;
+    while(auxE->aresta->omega!=aux){
+        auxE=auxE->proximo;
+    }    
+    return auxE->aresta->idAresta;
 }
 //=====================================================
 // Função que pega a próxima aresta na estrela de 
 // entrada de um vértice 
 //=====================================================
 int GAproxEntrada(Grafo grafo, int vertice, int aresta){
-    int i;
-    for(i=0;i<grafo->NumAre;i++){
-        if((grafo->aresta[i].y==vertice)&&(grafo->aresta[i].id>aresta))
-            return grafo->aresta[i].id;
+    ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
+    aux=findVertice(grafo,vertice);
+    ListaArestas auxA = (ListaArestas) malloc(sizeof(ListaArestas));
+    auxA=findAresta(grafo,aresta);
+    ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
+    auxE=aux->estrela;
+    while(auxE->aresta!=auxA){
+        auxE->proximo;
     }
-    return 0;
+    while(auxE->proximo->aresta->omega!=aux){
+        auxE->proximo;
+    }
+    auxE->proximo->aresta->idAresta;
 }
 //=====================================================
 // Função que pega a primeira aresta na estrela de 
@@ -347,8 +399,13 @@ int GAproxEntrada(Grafo grafo, int vertice, int aresta){
 //=====================================================
 int GAprimaSaida(Grafo grafo, int vertice){
     ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
-    aux=findVertice(vertice);
-    return aux->estrela->aresta->idAresta;
+    aux=findVertice(grafo,vertice);
+    ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
+    auxE=aux->estrela;
+    while(auxE->aresta->alfa!=aux){
+        auxE=auxE->proximo;
+    }    
+    return auxE->aresta->idAresta;
 }
 //=====================================================
 // Função que pega a próxima aresta na estrela de 
@@ -356,15 +413,18 @@ int GAprimaSaida(Grafo grafo, int vertice){
 //=====================================================
 int GAproxSaida(Grafo grafo, int vertice, int aresta){
     ListaVertices aux = (ListaVertices) malloc(sizeof(ListaVertices));
-    aux=findVertice(vertice);
-    ListaArestas auxA= (ListaArestas) malloc (sizeof(ListaArestas));
-    auxA=findAresta(grafo, aresta);
-    ListaEstrelas auxE= (ListaEstrelas) malloc (sizeof(ListaEstrelas));
+    aux=findVertice(grafo,vertice);
+    ListaArestas auxA = (ListaArestas) malloc(sizeof(ListaArestas));
+    auxA=findAresta(grafo,aresta);
+    ListaEstrelas auxE = (ListaEstrelas) malloc(sizeof(ListaEstrelas));
     auxE=aux->estrela;
-    while (auxE->aresta<>auxA){
-        auxE=auxE->proximo;
+    while(auxE->aresta!=auxA){
+        auxE->proximo;
     }
-    return auxE->proximo->aresta->idAresta;
+    while(auxE->proximo->aresta->alfa!=aux){
+        auxE->proximo;
+    }
+    auxE->proximo->aresta->idAresta;
 }
 //=====================================================
 // Função que diz se a aresta é ou não um laço
@@ -402,7 +462,7 @@ int GVvizinho(Grafo grafo, int aresta, int vertice){
     ListaArestas aux= (ListaArestas) malloc (sizeof(ListaArestas));
     aux=findAresta(grafo, aresta);
     ListaVertices auxV = (ListaVertices) malloc(sizeof(ListaVertices));
-    auxV=findVertice(vertice);
+    auxV=findVertice(grafo,vertice);
     if (aux->alfa=auxV){
         return aux->alfa->idVertice;
     }else{
